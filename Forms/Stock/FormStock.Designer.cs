c2e4d0056a68e844302e9f49636ec6523149115f
Peter Adeojo <PAdeojo@ggc.edu>
2020-07-18 03:46:46 -0400

    Fixed the Stock Page
    Added the functionality

---
+++ Forms/Stock/FormStock.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace InventoryApp.Forms
+{
+    partial class FormStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EquipmentListBox = new System.Windows.Forms.ListBox();
+            this.DisplayAllBTN = new FontAwesome.Sharp.IconButton();
+            this.DisplayDesktopBtn = new FontAwesome.Sharp.IconButton();
+            this.DisplayLaptopBtn = new FontAwesome.Sharp.IconButton();
+            this.DisplayPhoneBtn = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // EquipmentListBox
+            // 
+            this.EquipmentListBox.FormattingEnabled = true;
+            this.EquipmentListBox.ItemHeight = 16;
+            this.EquipmentListBox.Location = new System.Drawing.Point(483, 22);
+            this.EquipmentListBox.Name = "EquipmentListBox";
+            this.EquipmentListBox.Size = new System.Drawing.Size(274, 340);
+            this.EquipmentListBox.TabIndex = 0;
+            this.EquipmentListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // DisplayAllBTN
+            // 
+            this.DisplayAllBTN.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.DisplayAllBTN.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.DisplayAllBTN.IconColor = System.Drawing.Color.Black;
+            this.DisplayAllBTN.IconSize = 16;
+            this.DisplayAllBTN.Location = new System.Drawing.Point(60, 22);
+            this.DisplayAllBTN.Name = "DisplayAllBTN";
+            this.DisplayAllBTN.Rotation = 0D;
+            this.DisplayAllBTN.Size = new System.Drawing.Size(141, 64);
+            this.DisplayAllBTN.TabIndex = 1;
+            this.DisplayAllBTN.Text = "Display All";
+            this.DisplayAllBTN.UseVisualStyleBackColor = true;
+            this.DisplayAllBTN.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // DisplayDesktopBtn
+            // 
+            this.DisplayDesktopBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.DisplayDesktopBtn.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.DisplayDesktopBtn.IconColor = System.Drawing.Color.Black;
+            this.DisplayDesktopBtn.IconSize = 16;
+            this.DisplayDesktopBtn.Location = new System.Drawing.Point(60, 110);
+            this.DisplayDesktopBtn.Name = "DisplayDesktopBtn";
+            this.DisplayDesktopBtn.Rotation = 0D;
+            this.DisplayDesktopBtn.Size = new System.Drawing.Size(141, 64);
+            this.DisplayDesktopBtn.TabIndex = 2;
+            this.DisplayDesktopBtn.Text = "Display Desktop";
+            this.DisplayDesktopBtn.UseVisualStyleBackColor = true;
+            this.DisplayDesktopBtn.Click += new System.EventHandler(this.DisplayDesktopBtn_Click);
+            // 
+            // DisplayLaptopBtn
+            // 
+            this.DisplayLaptopBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.DisplayLaptopBtn.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.DisplayLaptopBtn.IconColor = System.Drawing.Color.Black;
+            this.DisplayLaptopBtn.IconSize = 16;
+            this.DisplayLaptopBtn.Location = new System.Drawing.Point(60, 206);
+            this.DisplayLaptopBtn.Name = "DisplayLaptopBtn";
+            this.DisplayLaptopBtn.Rotation = 0D;
+            this.DisplayLaptopBtn.Size = new System.Drawing.Size(141, 64);
+            this.DisplayLaptopBtn.TabIndex = 3;
+            this.DisplayLaptopBtn.Text = "Display Laptop";
+            this.DisplayLaptopBtn.UseVisualStyleBackColor = true;
+            this.DisplayLaptopBtn.Click += new System.EventHandler(this.DisplayLaptopBtn_Click);
+            // 
+            // DisplayPhoneBtn
+            // 
+            this.DisplayPhoneBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.DisplayPhoneBtn.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.DisplayPhoneBtn.IconColor = System.Drawing.Color.Black;
+            this.DisplayPhoneBtn.IconSize = 16;
+            this.DisplayPhoneBtn.Location = new System.Drawing.Point(60, 298);
+            this.DisplayPhoneBtn.Name = "DisplayPhoneBtn";
+            this.DisplayPhoneBtn.Rotation = 0D;
+            this.DisplayPhoneBtn.Size = new System.Drawing.Size(141, 64);
+            this.DisplayPhoneBtn.TabIndex = 4;
+            this.DisplayPhoneBtn.Text = "Display Phone";
+            this.DisplayPhoneBtn.UseVisualStyleBackColor = true;
+            this.DisplayPhoneBtn.Click += new System.EventHandler(this.DisplayPhoneBtn_Click);
+            // 
+            // FormStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DisplayPhoneBtn);
+            this.Controls.Add(this.DisplayLaptopBtn);
+            this.Controls.Add(this.DisplayDesktopBtn);
+            this.Controls.Add(this.DisplayAllBTN);
+            this.Controls.Add(this.EquipmentListBox);
+            this.Name = "FormStock";
+            this.Text = "FormStock";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox EquipmentListBox;
+        private FontAwesome.Sharp.IconButton DisplayAllBTN;
+        private FontAwesome.Sharp.IconButton DisplayDesktopBtn;
+        private FontAwesome.Sharp.IconButton DisplayLaptopBtn;
+        private FontAwesome.Sharp.IconButton DisplayPhoneBtn;
+    }
+}
---
+++ Forms/Stock/FormStock.cs
@@ -0,0 +1,90 @@
+﻿using InventoryLibrary.DataAccess;
+using InventoryLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryApp.Forms
+{
+    public partial class FormStock : Form
+    {
+        List<EquipmentModel> equipment = new List<EquipmentModel>();
+        public FormStock()
+        {
+            InitializeComponent();
+            LoadEquipmentList();
+        }
+
+
+        public void LoadEquipmentList()
+        {
+            EquipmentListBox.DataSource = null;
+            EquipmentListBox.Items.Clear();
+            equipment = EquipmentSetup.LoadEquipment();
+            WireUpEquipmentList();
+        }
+
+        public void LoadDesktopList()
+        {
+            EquipmentListBox.DataSource = null;
+            EquipmentListBox.Items.Clear();
+            equipment = EquipmentSetup.LoadDesktop();
+            WireUpEquipmentList();
+        }
+
+        public void LoadLaptopList()
+        {
+            EquipmentListBox.DataSource = null;
+            EquipmentListBox.Items.Clear();
+            equipment = EquipmentSetup.LoadLaptop();
+            WireUpEquipmentList();
+        }
+
+        public void LoadPhoneList()
+        {
+            EquipmentListBox.DataSource = null;
+            EquipmentListBox.Items.Clear();
+            equipment = EquipmentSetup.LoadPhone();
+            WireUpEquipmentList();
+        }
+
+        public void WireUpEquipmentList()
+        {
+            EquipmentListBox.DataSource = null;
+            EquipmentListBox.DataSource = equipment;
+            EquipmentListBox.DisplayMember = "MakeModel" ;
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            LoadEquipmentList();
+        }
+
+        private void DisplayDesktopBtn_Click(object sender, EventArgs e)
+        {
+            LoadDesktopList();
+        }
+
+        private void DisplayLaptopBtn_Click(object sender, EventArgs e)
+        {
+            LoadLaptopList();
+        }
+
+        private void DisplayPhoneBtn_Click(object sender, EventArgs e)
+        {
+            LoadPhoneList();
+        }
+    }
+}
